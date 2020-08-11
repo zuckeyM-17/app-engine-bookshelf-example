fdcca7a564e3e4bb44eae100d52b8ecd17b66ad9
zuckeyM-17 <k.matsumura0314@gmail.com>
2020-08-11 14:30:29 +0900

    implement Hello World

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
